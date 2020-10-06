caaacf9ce41559fdbb2b3ce50230edbe11f8664a
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2020-10-06 11:34:27 -0300

    iniciei a programação a logica

--- code/javascript/main.js
+++ code/javascript/main.js
@@ -1,9 +1,19 @@
 voltagem = document.getElementById("txt_volts").value;
-
-window.alert("voltagem");
-
-
+tela = document.getElementById("tela").value;
 function calcular(impedancia)
 {
-    
+    switch (impedancia) {
+        case 4:
+            window.alert(voltagem);
+            break;
+        case 6:
+            window.alert(voltagem);
+            break;
+        case 8:
+            window.alert(voltagem);
+            break;
+        case 16:
+            window.alert(voltagem);
+            break;
+    }
 }
