3442c70c62eff77a45e712165c9207634611dc6c
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2020-10-10 21:25:14 -0300

    comentários

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
  GUI application to calculate how much power in watts a given amplifier is putting out 
 
 it is supposed to be a mobile app
+
+***********************************************************
+ * (c) 2020 Pedro dos santos, são paulo, brasil           *
+ * O objetivo desse código é ser parte de um app mobile   *
+ * mas está escrito de forma horrível, porem funcional    *
+***********************************************************
--- code/javascript/main.js
+++ code/javascript/main.js
@@ -1,16 +1,23 @@
+/**********************************************************
+ * (c) 2020 Pedro dos santos, são paulo, brasil           *
+ * O objetivo desse código é ser parte de um app mobile   *
+ * mas está escrito de forma horrível, porem funcional    *
+***********************************************************/
 let voltagem;
 let potencia;
 let checkbox;
 let corpo;
 let txtvolts;
 
+//num precisa dessa linha, mas vai que...
 checkbox = document.getElementById("darkmode");
 
 
-
+// faz o calculo da potencia recebendo a voltagem da saída do amplificador
 function calcular(impedancia)
 {
     voltagem = document.getElementById("txtvolts").value;
+    //o calculo em si
     switch (impedancia)
     {
         case 4:
@@ -26,17 +33,22 @@ function calcular(impedancia)
             potencia = voltagem * voltagem / impedancia+" Watts";
             break;
     }
+    //substitui o titulo no h1 pelo resultado
     document.getElementById("tela").innerText = potencia;
 }
 
-
+//implementa o darkmode
 function darkmode()
 {
+    //atualiza as variáveis
     checkbox = document.getElementById("darkmode");
     corpo = document.getElementById("corpo");
     txtvolts = document.getElementById("txtvolts");
+
+    //da utilidade a chave de darkmode
     if(checkbox.checked == true)
     {
+        //modo escuro, alterando css e memoria local da maquina do usuário
         localStorage.setItem("mode", "ativado");
         corpo.style.backgroundColor = "#000000";
         txtvolts.style.backgroundColor = "#ffffff";
@@ -44,6 +56,7 @@ function darkmode()
     }
     else
     {
+        //modo claro revertendo tudo la de cima
         localStorage.setItem("mode", "desativado");
         corpo.style.backgroundColor = "#ffffff";
         txtvolts.style.backgroundColor = "#ffffff";
@@ -52,7 +65,7 @@ function darkmode()
 }
 
 
-
+//checa a ultima posição da chave de darkmode
 if(localStorage.mode == "ativado")
 {
     checkbox.checked = true;
@@ -62,4 +75,6 @@ else
     checkbox.checked = false;
 }
 
+//chama a função darkmode, quebrei a cabeça pra fazer essa gambiarra
+//sendo que era só usar essa unica linha de código
 document.onload = darkmode();
